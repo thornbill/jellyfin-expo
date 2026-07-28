cb2b98ad84ed769f86854bb154760842250aab52
Bill Thornton <thornbill@users.noreply.github.com>
2026-07-28 10:11:22 -0400

    Fix comment indentation

--- components/__tests__/ServerListItem.test.js
+++ components/__tests__/ServerListItem.test.js
@@ -1,5 +1,5 @@
 /**
-* Copyright (c) 2026 Jellyfin Contributors
+ * Copyright (c) 2026 Jellyfin Contributors
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
